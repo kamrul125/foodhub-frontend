2931f1027dd2009c3dae0064a2261753c28ae2b3
Md kamruzzaman <kamrul.bubtr@gmail.com>
2026-03-07 19:47:04 +0600

    updat code

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  // localhost পরিবর্তন করে আপনার ব্যাকএন্ডের Vercel লিঙ্কটি এখানে দিন
-  baseURL: "https://foodhub-backend.vercel.app/api", 
+  // এটি অটোমেটিক চেক করবে আপনি লোকালহোস্টে আছেন কি না
+  baseURL: window.location.hostname === "localhost" 
+    ? "http://localhost:5000/api" 
+    : "https://foodhub-backend.vercel.app/api", 
 });
 
 api.interceptors.request.use((config) => {
